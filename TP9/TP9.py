8a8a03d4e8e756fa2dbd92ec825ece1d9e1827f1
Manuel Bedoya Alberici <manualberici@gmail.com>
2024-08-26 15:32:43 +0100

    TP9b eliminado, comentario de funciones, corrección del tamaño inicial de la imagen

--- TP9/TP9.py
+++ TP9/TP9.py
@@ -2,25 +2,25 @@ import cv2
 import numpy as np
 import math
 
-# Initialize variables
+# Variables
 points = []
 mPoints = []
-shelves_height = 2.1  # Meters
-shelves_width = 1.1   # Meters
-original_image = cv2.imread('estantes.jpg')  # Load image
+shelves_height = 2.1        # Metros reales
+shelves_width = 1.1         # Metros reales
+original_image = cv2.imread('estantes.jpg')
 image = original_image.copy()
 warped_image = None
 warped_copy = None
-selecting_points = True
-oneTimeFlag = True
+selecting_points = True     # Flag para la medición de distancias
+oneTimeFlag = True          # Flag para elegir el tamaño de la estantería solo una vez
 
-# Function to handle mouse click events on the original image
+# Función para elegir las esquinas de la estantería
 def click_event(event, x, y, flags, params):
     global points, image, warped_image, warped_copy, oneTimeFlag
 
     if event == cv2.EVENT_LBUTTONDOWN and oneTimeFlag:
         points.append((x, y))
-        cv2.circle(image, (x, y), 3, (0, 0, 255), -1)  # Draw a red circle on the clicked point
+        cv2.circle(image, (x, y), 3, (0, 0, 255), -1) 
         cv2.imshow('image', image)
 
         if len(points) == 4:
@@ -31,18 +31,18 @@ def click_event(event, x, y, flags, params):
             warped_image = cv2.warpPerspective(original_image, H, (w, h))
             warped_copy = warped_image.copy()
             cv2.imshow('Warped Image', warped_image)
-            cv2.setMouseCallback('Warped Image', measure_distance)  # Set callback for the warped image
+            cv2.setMouseCallback('Warped Image', measure_distance)
             points = []
             print("\nSeleccione dos puntos para medir la distancia entre ellos.")
             oneTimeFlag = False
 
-# Function to measure distance between two points on the warped image
+# Función que mide la distancia entre dos puntos de la imagen
 def measure_distance(event, x, y, flags, param):
     global mPoints, warped_copy, selecting_points
 
     if event == cv2.EVENT_LBUTTONDOWN and selecting_points:
         mPoints.append((x, y))
-        cv2.circle(warped_copy, (x, y), 3, (0, 0, 255), -1)  # Draw a red circle on the clicked point
+        cv2.circle(warped_copy, (x, y), 3, (0, 0, 255), -1)
         cv2.imshow('Warped Image', warped_copy)
 
         if len(mPoints) == 2:
@@ -53,22 +53,20 @@ def measure_distance(event, x, y, flags, param):
             print("Presione 'r' para seleccionar dos puntos más o 'Esc' para salir.")
             selecting_points = False
 
-# Display the original image and set the mouse callback function
 cv2.imshow('image', image)
 cv2.setMouseCallback('image', click_event)
 
 print("Seleccione las 4 esquinas de la estantería.")
 
-# Main loop to listen for key presses
 while True:
     k = cv2.waitKey(1) & 0xFF
-    if k == ord('r'):  # Reset for new point selection
+    if k == ord('r') and oneTimeFlag == False:  
         warped_copy = warped_image.copy()
         cv2.imshow('Warped Image', warped_copy)
         print("Seleccione 2 nuevos puntos.")
         mPoints = []
         selecting_points = True
-    elif k == 27:  # Press 'Esc' to exit
+    elif k == 27:
         break
 
 cv2.destroyAllWindows()
